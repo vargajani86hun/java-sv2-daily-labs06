17844567858fc9e9cb74a971399dea448bc8cd96
JaniVarga86HUN <jani.varga86@gmail.com>
2021-11-15 15:47:49 +0100

    Ready.

---
+++ week06-lab-tasks/src/main/java/day01/Main.java
@@ -0,0 +1,21 @@
+package day01;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        PositiveNumberContainer pnc = new PositiveNumberContainer();
+        Scanner scr = new Scanner(System.in);
+        double number;
+        do {
+            System.out.println("Adjon meg egy valós számot:");
+            number = scr.nextDouble();
+            if (number > 0) {
+                pnc.addNumber(number);
+            }
+        } while (number > 0);
+        for (double d : pnc.getNumbers()) {
+            System.out.print(d + ", ");
+        }
+    }
+}
